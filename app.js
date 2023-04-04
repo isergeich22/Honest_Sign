469d3fcebb45479126f7abcc2d30e1ebf1336321
isergeich22 <skiing1r@gmail.com>
2023-04-04 23:13:43 +0300

    created route for wildberries

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exl = require('exceljs')
 const fs = require('fs')
 const cio = require('cheerio')
 const app = express()
+const { convertCsvToXlsx } = require ('@aternus/csv-to-xlsx')
 
 app.use('/home', express.static(__dirname + '/public/index.html'))
 
@@ -393,12 +394,22 @@ app.get('/wildberries', async function(req, res){
     const difference = []
     const wb_orders = []
     const nat_cat = []
+    const vendors = []
+    const names = []
     const ozon = []
+
+    const colors = ['БЕЖЕВЫЙ', 'БЕЛЫЙ', 'БИРЮЗОВЫЙ', 'БОРДОВЫЙ', 'БРОНЗОВЫЙ', 'ВАНИЛЬ', 'ВИШНЯ', 'ГОЛУБОЙ', 'ЖЁЛТЫЙ', 'ЗЕЛЁНЫЙ', 'ЗОЛОТОЙ', 'ИЗУМРУДНЫЙ',
+                        'КАПУЧИНО', 'КИРПИЧНЫЙ', 'КОРАЛЛОВЫЙ', 'КОРИЧНЕВЫЙ', 'КРАСНЫЙ', 'ЛАЙМ', 'ЛЕОПАРД', 'МАЛИНОВЫЙ', 'МЕДНЫЙ', 'МОЛОЧНЫЙ', 'МЯТНЫЙ', 'ОЛИВКОВЫЙ', 'ОРАНЖЕВЫЙ',
+                        'ПЕСОЧНЫЙ', 'ПЕРСИКОВЫЙ', 'ПУРПУРНЫЙ', 'РАЗНОЦВЕТНЫЙ', 'РОЗОВО-БЕЖЕВЫЙ', 'РОЗОВЫЙ', 'СВЕТЛО-БЕЖЕВЫЙ', 'СВЕТЛО-ЗЕЛЕНЫЙ', 'СВЕТЛО-КОРИЧНЕВЫЙ', 'СВЕТЛО-РОЗОВЫЙ',
+                        'СВЕТЛО-СЕРЫЙ', 'СВЕТЛО-СИНИЙ', 'СВЕТЛО-ФИОЛЕТОВЫЙ', 'СЕРЕБРЯНЫЙ', 'СЕРО-ЖЕЛТЫЙ', 'СЕРО-ГОЛУБОЙ', 'СЕРЫЙ', 'СИНИЙ', 'СИРЕНЕВЫЙ', 'ЛИЛОВЫЙ', 'СЛИВОВЫЙ',
+                        'ТЕМНО-БЕЖЕВЫЙ', 'ТЕМНО-ЗЕЛЕНЫЙ', 'ТЕМНО-КОРИЧНЕВЫЙ', 'ТЕМНО-РОЗОВЫЙ', 'ТЕМНО-СЕРЫЙ', 'ТЕМНО-СИНИЙ', 'ТЕМНО-ФИОЛЕТОВЫЙ', 'ТЕРРАКОТОВЫЙ', 'ФИОЛЕТОВЫЙ',
+                        'ФУКСИЯ', 'ХАКИ', 'ЧЕРНЫЙ', 'ШОКОЛАДНЫЙ'
+                        ]
 
     const wb = new exl.Workbook()
 
     const hsFile = './public/Краткий отчет.xlsx'
-    const ozonFile = './public/products.csv'
+    const ozonFile = './public/products.xlsx'
     const wbFile = './public/wildberries/new.xlsx'
 
     let html = ''
@@ -422,32 +433,331 @@ app.get('/wildberries', async function(req, res){
     c12.eachCell(c => {
         wb_orders.push(c.value)
     })
-    
-    // nat_cat.forEach(elem => {
-    //     html += `<p>${elem}</p>`
-    // })
 
-    await wb.csv.readFile(ozonFile)
+    await wb.xlsx.readFile(ozonFile)
 
-    ws => {
-        ozon.push((ws.Артикул).toString().replace(`'`, ``))
+    const ws_ = wb.getWorksheet('Worksheet')
+
+    const c1 = ws_.getColumn(1)
+    const c6 = ws_.getColumn(6)
+
+    c1.eachCell(c => {
+        vendors.push(c.value.replace(`'`,``))
+    })
+
+    c6.eachCell(c => {
+        names.push(c.value.trim())
+    })
+
+    wb_orders.forEach(elem => {
+        if(vendors.indexOf(elem) >= 0){
+            let index = vendors.indexOf(elem)
+            ozon.push(names[index])
+            // console.log(typeof index)
+        }
+    })
+
+    ozon.forEach(elem => {
+        if(nat_cat.indexOf(elem) < 0) {
+            difference.push(elem)
+        }
+    })
+
+    async function createImport(array) {
+
+        const fileName = './public/IMPORT_TNVED_6302 (3).xlsx'
+        
+        const wb = new exl.Workbook()
+
+        await wb.xlsx.readFile(fileName)
+
+        const ws = wb.getWorksheet('IMPORT_TNVED_6302')
+
+        let cellNumber = 5
+
+        for(i = 0; i < array.length; i++) {
+            let size = ''            
+                ws.getCell(`A${cellNumber}`).value = '6302'
+                ws.getCell(`B${cellNumber}`).value = array[i]
+                ws.getCell(`C${cellNumber}`).value = 'Ивановский текстиль'
+                ws.getCell(`D${cellNumber}`).value = 'Артикул'
+                let index = names.indexOf(array[i])
+                ws.getCell(`E${cellNumber}`).value = vendors[index]
+                for(let c = 0; c < colors.length; c++) {
+                    str = colors[c].toLowerCase()
+                    elem = array[i].toLowerCase()
+                    if(elem.indexOf(str) >= 0) {
+                        ws.getCell(`G${cellNumber}`).value = colors[c].toUpperCase()
+                    }
+                }
+                ws.getCell(`H${cellNumber}`).value = 'ВЗРОСЛЫЙ'
+
+                if(array[i].indexOf('Постельное') >= 0 || array[i].indexOf('Детское') >= 0) {
+                    ws.getCell(`F${cellNumber}`).value = 'КОМПЛЕКТ'
+                }
+                
+                if(array[i].indexOf('Простыня') >= 0) {
+                    if(array[i].indexOf('на резинке') >= 0) {
+                        ws.getCell(`F${cellNumber}`).value = 'ПРОСТЫНЯ НА РЕЗИНКЕ'
+                    } else {
+                        ws.getCell(`F${cellNumber}`).value = 'ПРОСТЫНЯ'
+                    }
+                }
+                if(array[i].indexOf('Пододеяльник') >= 0) {
+                    ws.getCell(`F${cellNumber}`).value = 'ПОДОДЕЯЛЬНИК С КЛАПАНОМ'
+                }
+                if(array[i].indexOf('Наволочка') >= 0) {
+                    if(array[i].indexOf('50х70') >=0 || array[i].indexOf('40х60') >= 0 || array[i].indexOf('50 х 70') >=0 || array[i].indexOf('40 х 60') >= 0) {
+                        ws.getCell(`F${cellNumber}`).value = 'НАВОЛОЧКА ПРЯМОУГОЛЬНАЯ'
+                    } else {
+                        ws.getCell(`F${cellNumber}`).value = 'НАВОЛОЧКА КВАДРАТНАЯ'
+                    }
+                }
+                if(array[i].indexOf('Наматрасник') >= 0) {
+                    ws.getCell(`F${cellNumber}`).value = 'НАМАТРАСНИК'
+                }
+                if(array[i].indexOf('страйп-сатин') >= 0 || array[i].indexOf('страйп сатин') >= 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'СТРАЙП-САТИН'
+                }
+                if(array[i].indexOf('твил-сатин') >= 0 || array[i].indexOf('твил сатин') >= 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'ТВИЛ-САТИН'
+                }
+                if(array[i].indexOf('тенсел') >= 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'ТЕНСЕЛЬ'
+                }
+                if(array[i].indexOf('бяз') >= 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'БЯЗЬ'
+                }
+                if(array[i].indexOf('поплин') >= 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'ПОПЛИН'
+                }
+                if(array[i].indexOf('сатин') >= 0 && array[i].indexOf('-сатин') < 0 && array[i].indexOf('п сатин') < 0 && array[i].indexOf('л сатин') < 0 && array[i].indexOf('сатин-') < 0 && array[i].indexOf('сатин ж') < 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'САТИН'
+                }
+                if(array[i].indexOf('вареный') >= 0 || array[i].indexOf('варёный') >= 0 || array[i].indexOf('вареного') >= 0 || array[i].indexOf('варёного') >= 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'ХЛОПКОВАЯ ТКАНЬ'
+                }
+                if(array[i].indexOf('сатин-жаккард') >= 0 || array[i].indexOf('сатин жаккард') >= 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'САТИН-ЖАККАРД'
+                }
+                if(array[i].indexOf('страйп-микрофибр') >= 0 || array[i].indexOf('страйп микрофибр') >= 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'МИКРОФИБРА'
+                }
+                if(array[i].indexOf('шерст') >= 0) {
+                    ws.getCell(`I${cellNumber}`).value = 'ПОЛИЭФИР'
+                }
+
+                if(array[i].indexOf('тенсел') >= 0) {ws.getCell(`J${cellNumber}`).value = '100% Эвкалипт'}
+                else if(array[i].indexOf('шерст') >= 0) {ws.getCell(`J${cellNumber}`).value = '100% Полиэстер'}
+                else {ws.getCell(`J${cellNumber}`).value = '100% Хлопок'}
+
+                //Вставка размера начало
+                
+                if(array[i].indexOf('Постельное') >= 0) {
+                    if(array[i].indexOf('1,5 спальное') >= 0 || array[i].indexOf('1,5 спальный') >= 0) {
+                        size = '1,5 спальное'
+                        if(array[i].indexOf('на резинке') >= 0) {
+                            size += ' на резинке'
+                            for(let k = 40; k < 305; k+=5) {
+                                for(let l = 40; l < 305; l+=5) {
+                                    if(array[i].indexOf(` ${k.toString()}х${l.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()}`) >= 0) {
+                                        for(let j = 10; j < 50; j+=10) {
+                                            if(array[i].indexOf(` ${k.toString()}х${l.toString()}х${j.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()} х ${j.toString()}`) >= 0) {
+                                                size += ` ${k.toString()}х${l.toString()}х${j.toString()}`
+                                                ws.getCell(`K${cellNumber}`).value = size
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if(array[i].indexOf('с наволочками 50х70') >= 0) {
+                            size += ' с наволочками 50х70'
+                            ws.getCell(`K${cellNumber}`).value = size
+                        } else {
+                            ws.getCell(`K${cellNumber}`).value = size
+                        }
+                    }
+                    if(array[i].indexOf('2 спальное') >= 0 || array[i].indexOf('2 спальный') >= 0) {
+                        size = '2 спальное'
+                        if(array[i].indexOf('с Евро') >= 0) {
+                            size += ' с Евро простыней'
+                        }
+                        if(array[i].indexOf('на резинке') >= 0) {
+                            size += ' на резинке'
+                            for(let k = 40; k < 305; k+=5) {
+                                for(let l = 40; l < 305; l+=5) {
+                                    if(array[i].indexOf(` ${k.toString()}х${l.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()}`) >= 0) {
+                                        for(let j = 10; j < 50; j+=10) {
+                                            if(array[i].indexOf(` ${k.toString()}х${l.toString()}х${j.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()} х ${j.toString()}`) >= 0) {
+                                                size += ` ${k.toString()}х${l.toString()}х${j.toString()}`
+                                                ws.getCell(`K${cellNumber}`).value = size
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if(array[i].indexOf('с наволочками 50х70') >= 0) {
+                            size += ' с наволочками 50х70'
+                            ws.getCell(`K${cellNumber}`).value = size
+                        } else {
+                            ws.getCell(`K${cellNumber}`).value = size
+                        }
+                    }
+                    if(array[i].indexOf('Евро -') >= 0 || new_products[i].indexOf('евро -') >= 0 || array[i].indexOf('Евро на') >= 0 || array[i].indexOf('евро на') >= 0) {
+                        size = 'Евро'
+                        if(array[i].indexOf('на резинке') >= 0) {
+                            size += ' на резинке'
+                            for(let k = 40; k < 305; k+=5) {
+                                for(let l = 40; l < 305; l+=5) {
+                                    if(array[i].indexOf(` ${k.toString()}х${l.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()}`) >= 0) {
+                                        for(let j = 10; j < 50; j+=10) {
+                                            if(array[i].indexOf(` ${k.toString()}х${l.toString()}х${j.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()} х ${j.toString()}`) >= 0) {
+                                                size += ` ${k.toString()}х${l.toString()}х${j.toString()}`
+                                                ws.getCell(`K${cellNumber}`).value = size
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if(array[i].indexOf('с наволочками 50х70') >= 0) {
+                            size += ' с наволочками 50х70'
+                            ws.getCell(`K${cellNumber}`).value = size
+                        } else {
+                            ws.getCell(`K${cellNumber}`).value = size
+                        }
+                    }
+                    if(array[i].indexOf('Евро Макси') >= 0 || array[i].indexOf('евро макси') >= 0 || array[i].indexOf('Евро макси') >= 0) {
+                        size = 'Евро Макси'
+                        if(array[i].indexOf('на резинке') >= 0) {
+                            size += ' на резинке'
+                            for(let k = 40; k < 305; k+=5) {
+                                for(let l = 40; l < 305; l+=5) {
+                                    if(array[i].indexOf(` ${k.toString()}х${l.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()}`) >= 0) {
+                                        for(let j = 10; j < 50; j+=10) {
+                                            if(array[i].indexOf(` ${k.toString()}х${l.toString()}х${j.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()} х ${j.toString()}`) >= 0) {
+                                                size += ` ${k.toString()}х${l.toString()}х${j.toString()}`
+                                                ws.getCell(`K${cellNumber}`).value = size
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if(array[i].indexOf('с наволочками 50х70') >= 0) {
+                            size += ' с наволочками 50х70'
+                            ws.getCell(`K${cellNumber}`).value = size
+                        } else {
+                            ws.getCell(`K${cellNumber}`).value = size
+                        }
+                    }
+                    if(array[i].indexOf('семейное') >= 0 || array[i].indexOf('семейный') >= 0) {
+                        size = 'семейное'
+                        if(array[i].indexOf('на резинке') >= 0) {
+                            size += ' на резинке'
+                            for(let k = 40; k < 305; k+=5) {
+                                for(let l = 40; l < 305; l+=5) {
+                                    if(array[i].indexOf(` ${k.toString()}х${l.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()}`) >= 0) {
+                                        for(let j = 10; j < 50; j+=10) {
+                                            if(array[i].indexOf(` ${k.toString()}х${l.toString()}х${j.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()} х ${j.toString()}`) >= 0) {
+                                                size += ` ${k.toString()}х${l.toString()}х${j.toString()}`
+                                                ws.getCell(`K${cellNumber}`).value = size
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if(array[i].indexOf('с наволочками 50х70') >= 0) {
+                            size += ' с наволочками 50х70'
+                            ws.getCell(`K${cellNumber}`).value = size
+                        } else {
+                            ws.getCell(`K${cellNumber}`).value = size
+                        }
+                    }
+                } else {
+                    for(let k = 40; k < 305; k+=5) {
+                        for(let l = 40; l < 305; l+=5) {
+                            if(array[i].indexOf(` ${k.toString()}х${l.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()}`) >= 0) {
+                                size = `${k.toString()}х${l.toString()}`
+                                for(let j = 10; j < 50; j+=10) {
+                                    if(array[i].indexOf(` ${k.toString()}х${l.toString()}х${j.toString()}`) >= 0 || array[i].indexOf(` ${k.toString()} х ${l.toString()} х ${j.toString()}`) >= 0) {
+                                        size = `${k.toString()}х${l.toString()}х${j.toString()}`
+                                        ws.getCell(`K${cellNumber}`).value = size
+                                    } else {
+                                        ws.getCell(`K${cellNumber}`).value = size
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                //Вставка размера конец
+
+                ws.getCell(`L${cellNumber}`).value = '6302100001'
+                ws.getCell(`M${cellNumber}`).value = 'ТР ТС 017/2011 "О безопасности продукции легкой промышленности'
+                ws.getCell(`N${cellNumber}`).value = 'На модерации'                
+
+                cellNumber++
+
+        }
+
+        ws.unMergeCells('D2')
+
+        ws.getCell('E2').value = '13914'
+
+        ws.getCell('E2').fill = {
+            type: 'pattern',
+            pattern: 'solid',
+            fgColor:{argb:'E3E3E3'}
+        }
+
+        ws.getCell('E2').font = {
+            size: 10,
+            name: 'Arial'
+        }
+
+        ws.getCell('E2').alignment = {
+            horizontal: 'center',
+            vertical: 'bottom'
+        }        
+
+        const date_ob = new Date()
+
+        let month = date_ob.getMonth() + 1
+
+        let filePath = ''
+
+        month < 10 ? filePath = `./public/wildberries/IMPORT_TNVED_6302_${date_ob.getDate()}_0${month}` : filePath = `./public/wildberries/IMPORT_TNVED_6302_${date_ob.getDate()}_0${month}`
+
+        fs.access(`${filePath}.xlsx`, fs.constants.R_OK, async (err) => {
+            if(err) {
+                await wb.xlsx.writeFile(`${filePath}.xlsx`)
+            } else {
+                let count = 1
+                fs.access(`${filePath}_(1).xlsx`, fs.constants.R_OK, async (err) => {
+                    if(err) {
+                        await wb.xlsx.writeFile(`${filePath}_(1).xlsx`)
+                    } else {
+                        await wb.xlsx.writeFile(`${filePath}_(2).xlsx`)
+                    }
+                })
+                
+            }
+        })
+
     }
 
-    console.log(ozon)
+    createImport(difference)
 
-    // const _ws = wb.getWorksheet('products')
+    difference.forEach(elem => {
+        html += `<p>${elem}</p>`
+    })
 
-    // const c = _ws.getColumn(1)
-
-    // c.eachCell(cell => {
-    //     ozon.push((cell.value).toString().replace("'", ""))
-    // })
-
-    // ozon.forEach(elem => {
-    //     html += `<p>${elem}</p>`
-    // })
-
-    
+    res.send(html)
 
 })
 
